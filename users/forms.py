0f64965bbc65e6806af1102ad41fb15ec1583b98
Anton Golosnichenko <golosnichenko@artics.ru>
2020-10-13 00:18:28 +0300

    98. Форма регистрации

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,30 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Пароль неверный"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("Пользователь не существует"))
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=80, label="Имя")
+    last_name = forms.CharField(max_length=80, label="Фамилия")
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
+    password1 = forms.CharField(
+        widget=forms.PasswordInput, label="Подтверждение пароля"
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("Пользователь уже существует")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Пароли не совпадают")
+        else:
+            return password
--- users/views.py
+++ users/views.py
@@ -24,3 +24,15 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "Антон",
+        "last_name": "Голосниченко",
+        "email": "anton@golosnichenko.pro",
+    }
