837b0d0e33dc3427f1ba7702798c0bace610cd83
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-24 22:39:41 +0300

    139. Оформление профиля пользователя

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,16 @@ module.exports = {
       spacing: {
         "25vh": "25vh",
         "50vh": "50vh",
-        "75vh": "75vh"
+        "75vh": "75vh",
       },
-      borderWidth:{
-        "1/2": "0.5px"
-      }
+      borderWidth: {
+        "1/2": "0.5px",
+      },
+      minHeight: {
+        "75vh": "75vh",
+      },
     },
   },
   variants: {},
   plugins: [],
-}
+};
