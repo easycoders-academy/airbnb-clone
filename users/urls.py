34c85366ab9a79408a97676ecbda4853b5cfd821
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-26 02:31:54 +0300

    141. Изменение пароля

--- users/urls.py
+++ users/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     ),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
     path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
+    path("update-password/", views.UpdatePasswordView.as_view(), name="password"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from django.views import View
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
@@ -217,7 +218,6 @@ class UpdateProfileView(UpdateView):
     fields = (
         "first_name",
         "last_name",
-        "avatar",
         "gender",
         "bio",
         "birthdate",
@@ -227,3 +227,7 @@ class UpdateProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UpdatePasswordView(PasswordChangeView):
+    template_name = "users/update-password.html"
