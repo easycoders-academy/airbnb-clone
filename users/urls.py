5c4c03ab121d5c7d515c8641479114bb32c79a69
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-26 00:50:57 +0300

    140. Редактирование профиля

--- users/urls.py
+++ users/urls.py
@@ -4,13 +4,16 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login"),
-    path("login/github", views.github_login, name="github-login"),
-    path("login/github/callback", views.github_callback, name="github-callback"),
-    path("login/vk", views.vk_login, name="vk-login"),
-    path("login/vk/callback", views.vk_callback, name="vk-callback"),
-    path("logout", views.log_out, name="logout"),
-    path("signup", views.SignUpView.as_view(), name="signup"),
-    path("verify/<str:key>", views.complete_verification, name="complete-verification"),
-    path("<int:pk>", views.UserProfileView.as_view(), name="profile"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/github/", views.github_login, name="github-login"),
+    path("login/github/callback/", views.github_callback, name="github-callback"),
+    path("login/vk/", views.vk_login, name="vk-login"),
+    path("login/vk/callback/", views.vk_callback, name="vk-callback"),
+    path("logout/", views.log_out, name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path(
+        "verify/<str:key>/", views.complete_verification, name="complete-verification"
+    ),
+    path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.views import View
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -209,3 +209,21 @@ def vk_callback(request):
 class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
