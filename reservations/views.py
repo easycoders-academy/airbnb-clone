f25e5cd22ffb539b8de0188a1be733df7f1f37db
Anton Golosnichenko <golosnichenko@artics.ru>
2021-05-14 02:44:32 +0300

    169. Создание бронирования

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import datetime
+from django.views.generic import View
+from django.contrib import messages
+from rooms import models as room_models
+from . import models
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+
+class CreateException(Exception):
+    pass
+
+
+def create(request, room, year, month, day):
+    try:
+        date_obj = datetime.datetime(year, month, day)
+        room = room_models.Room.objects.get(pk=room)
+        models.BookedDay.objects.get(day=date_obj, reservation__room=room)
+        raise CreateException()
+    except (room_models.Room.DoesNotExist, CreateException):
+        messages.error(request, "Невозможно создать бронирование")
+        return redirect(reverse("core:home"))
+    except models.BookedDay.DoesNotExist:
+        reservation = models.Reservation.objects.create(
+            guest=request.user,
+            room=room,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
+        )
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
+
+class ReservationDetailView(View):
+    def get(self):
+        pass
