fde3fee521130edc02b6fb516884a1287a2fe073
Anton Golosnichenko <golosnichenko@artics.ru>
2021-05-14 20:50:22 +0300

    171. Информация о бронировании - часть 1

--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from rooms import models as room_models
@@ -30,7 +31,16 @@ def create(request, room, year, month, day):
 
 
 class ReservationDetailView(View):
-    def get(self, pk):
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
         reservation = models.Reservation.objects.get_or_none(pk=pk)
         if not reservation:
-            redirect(reverse("core:home"))
+            raise Http404()
+        if (
+            self.request.user != reservation.guest
+            and self.request.user != reservation.room.host
+        ):
+            raise Http404()
+        return render(
+            self.request, "reservations/detail.html", {"reservation": reservation}
+        )
