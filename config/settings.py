10f3521fa00ba0ec566dee546f3f9dcfea95df91
Anton Golosnichenko <golosnichenko@artics.ru>
2021-05-18 21:00:58 +0300

    192. PostgreSQL - часть 2

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
