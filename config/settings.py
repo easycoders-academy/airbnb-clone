aa727f15d5a589101f4fcd00bd70b36e6701d1b8
Anton Golosnichenko <golosnichenko@artics.ru>
2021-05-19 03:06:51 +0300

    196. Staticfiles

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,7 @@ if DEBUG is not True:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-easycoders"
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-central-1.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
